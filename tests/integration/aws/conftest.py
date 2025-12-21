1c749e01a94c0eecf642540c502c46a58aeb6d66
Abdellah Filali <abdellah.filali@stud.htwk-leipzig.de>
2025-12-21 19:47:21 +0100

    #72: update LocalStack test fixture to use HttpWaitStrategy and dynamic endpoint URL

--- tests/integration/aws/conftest.py
+++ tests/integration/aws/conftest.py
@@ -1,25 +1,25 @@
-import pytest
 import os
-
-from testcontainers.core.wait_strategies import LogMessageWaitStrategy
+import pytest
 from testcontainers.localstack import LocalStackContainer
+from testcontainers.core.wait_strategies import HttpWaitStrategy
 from src.core.aws.database.migrations import create_tables
 from src.core.aws.database.operations import DynamoDBOperations
 
 
 @pytest.fixture(scope="session")
 def dynamodb_setup():
-    """Start LocalStack for eu-central-1 with the specific table name."""
     os.environ["TESTCONTAINERS_RYUK_DISABLED"] = "true"
 
     container = LocalStackContainer("localstack/localstack:3.8.1").with_services(
         "dynamodb"
     )
 
-    container.waiting_for(LogMessageWaitStrategy("Ready."))
+    container.waiting_for(HttpWaitStrategy(4566).for_status_code(200))
 
     with container as ls:
-        endpoint_url = ls.get_url()
+        host = ls.get_container_host_ip()
+        port = ls.get_exposed_port(4566)
+        endpoint_url = f"http://{host}:{port}"
 
         os.environ["DYNAMODB_ENDPOINT_URL"] = endpoint_url
         os.environ["DYNAMODB_TABLE_NAME"] = "aura-historia-data"
